92ff812f5d8a8fe4823b03da05f8999366e42b2c
Pi Pi <diwas118151@gmail.com>
2023-09-13 11:02:49 +0700

    change vovan

---
+++ wwwroot/js/components/header.js
@@ -0,0 +1,6 @@
+const cartTrigger = document.querySelector('.cart-trigger')
+const cardModal = document.querySelector('.card-modal')
+
+cartTrigger.onclick = () => {
+   cardModal.style.opacity = 1
+}
