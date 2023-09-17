16e00044c7ab8e6d37bd67a8eebeb7b7bce347e3
Pi Pi <diwas118151@gmail.com>
2023-09-17 14:20:34 +0700

    55% Carousel1

---
+++ wwwroot/js/components/carousel1.js
@@ -0,0 +1,3 @@
+const carousel1PrevBtn = document.querySelector('.carousel1__slide-btn.prev')
+const carousel1NextBtn = document.querySelector('.carousel1__slide-btn.next')
+const carouselContainer = document.querySelector('.carousel1__container')
