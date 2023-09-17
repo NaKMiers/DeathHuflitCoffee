857bc4fd5cb9f4e459e880ee3cd79b2562c18bb1
Pi Pi <diwas118151@gmail.com>
2023-09-17 15:49:55 +0700

    completed Carousel1

--- wwwroot/js/components/carousel1.js
+++ wwwroot/js/components/carousel1.js
@@ -1,3 +1,20 @@
-const carousel1PrevBtn = document.querySelector('.carousel1__slide-btn.prev')
-const carousel1NextBtn = document.querySelector('.carousel1__slide-btn.next')
-const carouselContainer = document.querySelector('.carousel1__container')
+const prevBtn = document.querySelector('.carousel1__slide-btn.prev')
+const nextBtn = document.querySelector('.carousel1__slide-btn.next')
+const container = document.querySelector('.carousel1__container')
+const carouselItem = document.querySelector('.carousel1__item')
+
+let width = carouselItem.offsetWidth
+
+window.addEventListener('resize', () => {
+   width = carouselItem.offsetWidth
+})
+
+nextBtn.onclick = () => {
+   container.style.scrollBehavior = 'smooth'
+   container.scrollLeft = container.scrollLeft += width
+}
+
+prevBtn.onclick = () => {
+   container.style.scrollBehavior = 'smooth'
+   container.scrollLeft = container.scrollLeft -= width
+}
