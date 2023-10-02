fe041df865e9916ecc8d5b91a5d5f031af588dd8
Pi Pi <diwas118151@gmail.com>
2023-10-03 02:14:45 +0700

    null

--- Data/DeathWishCoffeeDbContext.cs
+++ Data/DeathWishCoffeeDbContext.cs
@@ -10,21 +10,21 @@ namespace DeathWishCoffee.Data
 
       }
 
-      public DbSet<Product> Products { get; set; }
-      public DbSet<Review> Reviews { get; set; }
-      public DbSet<OrderHistory> OrdersHistory { get; set; }
-      public DbSet<User> Users { get; set; }
+      public DbSet<Product>? Products { get; set; }
+      public DbSet<Review>? Reviews { get; set; }
+      public DbSet<OrderHistory>? OrdersHistory { get; set; }
+      public DbSet<User>? Users { get; set; }
 
-      public DbSet<FlavorProfile> FlavorProfiles { get; set; }
-      public DbSet<Image> Images { get; set; }
-      public DbSet<DeathWishCoffee.Models.Domain.Type> Types { get; set; }
-      public DbSet<Format> Formats { get; set; }
-      public DbSet<Roast> Roasts { get; set; }
-      public DbSet<Flavor> Flavors { get; set; }
-      public DbSet<Size> Sizes { get; set; }
-      public DbSet<InsideType> InsideTypes { get; set; }
-      public DbSet<DeathWishCoffee.Models.Domain.Attribute> Attributes { get; set; }
-      public DbSet<Detail> Details { get; set; }
-      public DbSet<Symbol> Symbols { get; set; }
+      public DbSet<FlavorProfile>? FlavorProfiles { get; set; }
+      public DbSet<Image>? Images { get; set; }
+      public DbSet<DeathWishCoffee.Models.Domain.Type>? Types { get; set; }
+      public DbSet<Format>? Formats { get; set; }
+      public DbSet<Roast>? Roasts { get; set; }
+      public DbSet<Flavor>? Flavors { get; set; }
+      public DbSet<Size>? Sizes { get; set; }
+      public DbSet<InsideType>? InsideTypes { get; set; }
+      public DbSet<DeathWishCoffee.Models.Domain.Attribute>? Attributes { get; set; }
+      public DbSet<Detail>? Details { get; set; }
+      public DbSet<Symbol>? Symbols { get; set; }
    }
 }
