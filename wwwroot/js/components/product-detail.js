e628cb8cd27198166aca4a19526906f5c637692f
trankhaa <144614010+trankhaa@users.noreply.github.com>
2023-10-12 08:30:29 +0700

    ht

--- wwwroot/js/components/product-detail.js
+++ wwwroot/js/components/product-detail.js
@@ -95,3 +95,71 @@ divC.addEventListener('click', function() {
     }
   });
 
+
+
+   // Lấy các phần tử div
+const divX = document.getElementById('KproductDetail_Size_x');
+const divY = document.getElementById('KproductDetail_Size_y');
+
+// Sử dụng biến để theo dõi trạng thái của div A
+let isDivXClicked = false;
+
+// Xử lý sự kiện khi click vào div A
+divX.addEventListener('click', function() {
+  if (!isDivXClicked) {
+    // Hiển thị div B và C khi div A chưa được click
+    divY.style.display = 'block';
+    // divC.style.display = 'block';
+
+    // Đánh dấu div A đã được click
+    isDivXClicked = true;
+  } else {
+    // Ẩn div B và C khi div A đã được click trước đó
+    divY.style.display = 'none';
+    // divC.style.display = 'none';
+
+    // Bỏ đánh dấu div A
+    isDivXClicked = false;
+  }
+
+  // Hiển thị lên là div đã chọn
+  divX.classList.add('selected');
+  
+  // Loại bỏ lớp 'selected' cho các div khác nếu có
+  if (divY.classList.contains('selected')) {
+    divY.classList.remove('selected');
+  }
+  // if (divC.classList.contains('selected')) {
+  //   divC.classList.remove('selected');
+  // }
+});
+
+// Xử lý sự kiện khi click vào div B
+divY.addEventListener('click', function() {
+    if (!isDivXClicked) {
+      // Hiển thị div B và C khi div A chưa được click
+      divX.style.display = 'block';
+      // divC.style.display = 'block';
+  
+      // Đánh dấu div A đã được click
+      isDivXClicked = true;
+    } else {
+      // Ẩn div B và C khi div A đã được click trước đó
+      divX.style.display = 'none';
+      // divC.style.display = 'none';
+  
+      // Bỏ đánh dấu div A
+      isDivXClicked = false;
+    }
+  
+    // Hiển thị lên là div đã chọn
+    divX.classList.add('selected');
+    
+    // Loại bỏ lớp 'selected' cho các div khác nếu có
+    if (divX.classList.contains('selected')) {
+      divY.classList.remove('selected');
+    }
+    // if (divC.classList.contains('selected')) {
+    //   divC.classList.remove('selected');
+    // }
+  });
