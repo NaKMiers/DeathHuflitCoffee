445cd0bc64f6c975633f17b658949d396d308b7d
UinLan <kimlanlongtuyen@gmail.com>
2023-10-13 13:16:00 +0700

    da lam theo yeu cau cua Khoa la fiter sort voi may nut 1 2 3 next co the submit hien du cac li trong tran dau

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -13,17 +13,22 @@ namespace DeathWishCoffee.Controllers
             _logger = logger;
         }
 
+
+
+
         public IActionResult Index(int id)
         {
             Console.WriteLine(id);
             return View();
         }
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
 }
+
+
+
--- wwwroot/js/components/products.js
+++ wwwroot/js/components/products.js
@@ -34,3 +34,35 @@ $(document).ready(function () {
       window.location.href = link; // Chuyển hướng đến đường link khi nhấn vào <li>
    });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+   var lis = document.querySelectorAll(".list-menu__item");
+
+   lis.forEach(function (li) {
+      li.addEventListener("click", function () {
+         // Lấy văn bản của mục li đã chọn
+         var selectedText = li.textContent;
+
+         // Thêm giá trị đã chọn vào trường tên "selected_item" của form
+         var form = document.querySelector("form");
+         var input = document.createElement("input");
+         input.type = "hidden";
+         input.name = "selected_item";
+         input.value = selectedText;
+         form.appendChild(input);
+
+         // Gửi form bằng phương thức "get"
+         form.submit();
+      });
+   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+   var selectElement = document.getElementById("SortBy");
+   var form = document.getElementById("myForm");
+
+   selectElement.addEventListener("change", function () {
+      // Submit form khi giá trị thay đổi
+      form.submit();
+   });
+});
