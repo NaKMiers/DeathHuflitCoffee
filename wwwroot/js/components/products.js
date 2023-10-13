6cbd01eb0d606d56a5f6ae0641cdd27502220700
UinLan <kimlanlongtuyen@gmail.com>
2023-10-13 13:58:38 +0700

    thay doi lai ten class cho products

--- wwwroot/js/components/products.js
+++ wwwroot/js/components/products.js
@@ -29,10 +29,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 $(document).ready(function () {
    // Chọn các thẻ <li> có class "grid__item scroll-trigger animate--slide-in"
-   $(".grid__item.scroll-trigger.animate--slide-in").click(function () {
-      var link = $(this).data("link"); // Lấy đường link từ thuộc tính data-link của <li>
-      window.location.href = link; // Chuyển hướng đến đường link khi nhấn vào <li>
-   });
+   $(".Products__grid-item.Products__scroll-trigger.Products__animate--slide-in").click(
+      function () {
+         var link = $(this).data("link"); // Lấy đường link từ thuộc tính data-link của <li>
+         window.location.href = link; // Chuyển hướng đến đường link khi nhấn vào <li>
+      }
+   );
 });
 
 document.addEventListener("DOMContentLoaded", function () {
