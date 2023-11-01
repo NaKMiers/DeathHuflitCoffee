e588d562a188eec2335c224e0359cccb8458e4ac
wuan204 <hokhanhdan1@gmail.com>
2023-11-01 16:33:21 +0700

    da hoan thanh hieu ung cho Quan-Card

--- wwwroot/js/components/cards.js
+++ wwwroot/js/components/cards.js
@@ -1,27 +1,35 @@
+const cardList = document.querySelector(".card-list");
 const cardBoxes = document.querySelectorAll(".card-box");
 const nextButton = document.querySelector(".nextcard-button");
 const prevButton = document.querySelector(".prevous-button");
 
-let currentIndex = 0;
+let currentSlide = 0;
 
 nextButton.addEventListener("click", () => {
-  cardBoxes[currentIndex].style.display = "none"; // Ẩn thẻ hiện tại
-
-  if (currentIndex < cardBoxes.length - 1) {
-    currentIndex++; // Tăng chỉ số nếu không phải thẻ cuối cùng
-  }
-
-  cardBoxes[currentIndex].style.display = "block"; // Hiển thị thẻ mới
+    if (currentSlide < cardBoxes.length - 1) {
+        currentSlide++;
+        updateSlide();
+    }
 });
 
 prevButton.addEventListener("click", () => {
-  cardBoxes[currentIndex].style.display = "none"; // Ẩn thẻ hiện tại
-
-  if (currentIndex > 0) {
-    currentIndex--; // Giảm chỉ số nếu không phải thẻ đầu tiên
-  }
-
-  cardBoxes[currentIndex].style.display = "block"; // Hiển thị thẻ mới
+    if (currentSlide > 0) {
+        currentSlide--;
+        updateSlide();
+    }
 });
 
+function updateSlide() {
+    cardList.style.transform = `translateX(-${currentSlide * 100}%)`;
 
+    cardBoxes.forEach((box, index) => {
+        if (index === currentSlide) {
+            box.classList.add("active");
+        } else {
+            box.classList.remove("active");
+        }
+    });
+}
+
+// Hiển thị thẻ đầu tiên khi trang web tải lên
+updateSlide();
