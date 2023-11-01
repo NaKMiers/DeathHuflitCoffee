14306d4b0d2b84178d38630c6e9440c99bdfc99a
wuan204 <hokhanhdan1@gmail.com>
2023-11-01 10:05:50 +0700

    asdasd

--- wwwroot/js/components/cards.js
+++ wwwroot/js/components/cards.js
@@ -1,26 +1,26 @@
-const cardSlide = document.querySelector('.card-slide');
-const cardList = document.querySelector('.card-list');
-const cardBoxes = document.querySelectorAll('.card-box');
-const sectionWidth = cardBoxes[0].offsetWidth; // Kích thước của mỗi phần
-let currentSection = 0; // Phần hiện tại
+// const cardSlide = document.querySelector('.card-slide');
+// const cardList = document.querySelector('.card-list');
+// const cardBoxes = document.querySelectorAll('.card-box');
+// const sectionWidth = cardBoxes[0].offsetWidth; // Kích thước của mỗi phần
+// let currentSection = 0; // Phần hiện tại
 
-function scrollToSection(section) {
-    currentSection = section;
-    const targetX = currentSection * sectionWidth;
-    cardSlide.scrollLeft = targetX;
-}
+// function scrollToSection(section) {
+//     currentSection = section;
+//     const targetX = currentSection * sectionWidth;
+//     cardSlide.scrollLeft = targetX;
+// }
 
-function scrollToPreviousSection() {
-    if (currentSection > 0) {
-        scrollToSection(currentSection - 1);
-    }
-}
+// function scrollToPreviousSection() {
+//     if (currentSection > 0) {
+//         scrollToSection(currentSection - 1);
+//     }
+// }
 
-function scrollToNextSection() {
-    if (currentSection < cardBoxes.length - 1) {
-        scrollToSection(currentSection + 1);
-    }
-}
+// function scrollToNextSection() {
+//     if (currentSection < cardBoxes.length - 1) {
+//         scrollToSection(currentSection + 1);
+//     }
+// }
 
-document.querySelector('.prev-button').addEventListener('click', scrollToPreviousSection);
-document.querySelector('.next-button').addEventListener('click', scrollToNextSection);
+// document.querySelector('.prev-button').addEventListener('click', scrollToPreviousSection);
+// document.querySelector('.next-button').addEventListener('click', scrollToNextSection);
