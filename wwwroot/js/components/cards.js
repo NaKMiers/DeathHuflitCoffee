de581ebd5873965de9b2327e1664cc06aebb8da7
wuan204 <hokhanhdan1@gmail.com>
2023-11-01 13:48:20 +0700

    da sua xong Quan-Card

--- wwwroot/js/components/cards.js
+++ wwwroot/js/components/cards.js
@@ -1,33 +1,56 @@
-// Lấy danh sách các thẻ li và nút Previous và Next
-const liItems = document.querySelectorAll('.card-box');
-const prevButton = document.querySelector('.prevous-button');
-const nextButton = document.querySelector('.nextcard-button');
+const cardList = document.querySelector(".card-list");
+const nextButton = document.querySelector(".nextcard-button");
+const prevButton = document.querySelector(".prevous-button");
 
-// Định nghĩa biến để theo dõi vị trí hiện tại của thẻ active
-let currentIndex = 0;
+let currentSlide = 0;
+let isMobileView = false; // Biến kiểm tra xem màn hình có đang ở chế độ mobile (nhỏ hơn 990px) hay không
 
-// Hàm để hiển thị thẻ li tương ứng với vị trí index
-function showLi(index) {
-    liItems.forEach((li, i) => {
-        if (i === index) {
-            li.classList.add('active');
-        } else {
-            li.classList.remove('active');
-        }
+nextButton.addEventListener("click", () => {
+    if (currentSlide < 2) { // Kiểm tra xem đã ở cuối danh sách chưa
+        currentSlide++;
+    } else {
+        currentSlide = 0; // Quay lại thẻ đầu tiên nếu ở cuối danh sách
+    }
+    updateSlide();
+});
+
+prevButton.addEventListener("click", () => {
+    if (currentSlide > 0) {
+        currentSlide--;
+    } else {
+        currentSlide = 2; // Quay lại thẻ cuối cùng nếu ở thẻ đầu tiên
+    }
+    updateSlide();
+});
+function updateSlide() {
+    const liElements = cardList.querySelectorAll(".card-box");
+    liElements.forEach((li, index) => {
+        if (index === currentSlide) {
+            li.classList.add("active"); // Hiển thị thẻ hiện tại
+        } 
     });
 }
+// Hiển thị thẻ đầu tiên khi trang web tải lên
+updateSlide();
+// Kiểm tra kích thước màn hình và chuyển giữa chế độ mobile và desktop
+function checkScreenSize() {
+    if (window.innerWidth < 990 && !isMobileView) {
+        // Chuyển sang chế độ mobile
+        isMobileView = true;
+        // Thực hiện chức năng chuyển trang hoặc thực hiện chức năng mobile ở đây
+    } else if (window.innerWidth >= 990 && isMobileView) {
+        // Quay lại chế độ desktop
+        isMobileView = false;
+        // Thực hiện chức năng desktop ở đây
 
-// Sự kiện khi nhấn nút Previous
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + liItems.length) % liItems.length;
-    showLi(currentIndex);
-});
+        // Hiển thị tất cả các thẻ li khi quay lại chế độ desktop
+        liElements.forEach((li) => {
+            li.style.display = "block";
+        });
+    }
+}
+// Thực hiện kiểm tra kích thước màn hình khi tải lại trang và cả khi thay đổi kích thước cửa sổ
+window.addEventListener("load", checkScreenSize);
+window.addEventListener("resize", checkScreenSize);
 
-// Sự kiện khi nhấn nút Next
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % liItems.length;
-    showLi(currentIndex);
-});
 
-// Hiển thị thẻ li đầu tiên khi trang web được tải
-showLi(currentIndex);
