943ff8e0ae6b6a1f5e030a9e482c7f9b6e68e04f
Pi Pi <diwas118151@gmail.com>
2023-11-06 01:01:07 +0700

    ok

--- wwwroot/js/components/header.js
+++ wwwroot/js/components/header.js
@@ -1,283 +1,283 @@
-$(document).ready(function () {
-   // cart-modal
-   const cartTrigger = document.querySelector('.cart-trigger')
-   const cardModal = document.querySelector('.cart-modal')
-   const cartModalContainer = document.querySelector('.cart-modal__container')
-   const closeCartModalBtn = document.querySelector('.cart-modal__close-btn')
+// $(document).ready(function () {
+//    // cart-modal
+//    const cartTrigger = document.querySelector('.cart-trigger')
+//    const cardModal = document.querySelector('.cart-modal')
+//    const cartModalContainer = document.querySelector('.cart-modal__container')
+//    const closeCartModalBtn = document.querySelector('.cart-modal__close-btn')
 
-   const showCart = () => {
-      cartTrigger.classList.add('show')
-      cardModal.style.display = 'block'
-      setTimeout(() => {
-         cardModal.style.opacity = 1
-         cartModalContainer.style.transform = 'translateX(0)'
-         cartModalContainer.style.opacity = 1
-      }, 0)
-   }
+//    const showCart = () => {
+//       cartTrigger.classList.add('show')
+//       cardModal.style.display = 'block'
+//       setTimeout(() => {
+//          cardModal.style.opacity = 1
+//          cartModalContainer.style.transform = 'translateX(0)'
+//          cartModalContainer.style.opacity = 1
+//       }, 0)
+//    }
 
-   const hideCart = () => {
-      cardModal.style.opacity = 0
-      cartModalContainer.style.transform = 'translateX(101%)'
-      cartModalContainer.style.opacity = 0
-      cartTrigger.classList.remove('show')
-      setTimeout(() => {
-         cardModal.style.display = 'none'
-      }, 510) // transition: 0.5s
-   }
+//    const hideCart = () => {
+//       cardModal.style.opacity = 0
+//       cartModalContainer.style.transform = 'translateX(101%)'
+//       cartModalContainer.style.opacity = 0
+//       cartTrigger.classList.remove('show')
+//       setTimeout(() => {
+//          cardModal.style.display = 'none'
+//       }, 510) // transition: 0.5s
+//    }
 
-   cartTrigger.onclick = () => {
-      !cartTrigger.className.includes('show') ? showCart() : hideCart()
-   }
+//    cartTrigger.onclick = () => {
+//       !cartTrigger.className.includes('show') ? showCart() : hideCart()
+//    }
 
-   closeCartModalBtn.onclick = () => {
-      hideCart()
-   }
+//    closeCartModalBtn.onclick = () => {
+//       hideCart()
+//    }
 
-   cardModal.onclick = e => {
-      if (cartModalContainer && !cartModalContainer.contains(e.target)) {
-         hideCart()
-      }
-   }
-})
+//    cardModal.onclick = e => {
+//       if (cartModalContainer && !cartModalContainer.contains(e.target)) {
+//          hideCart()
+//       }
+//    }
+// })
 
-$(document).ready(function () {
-   // sub-menu coffee
-   const subMenuCoffeeTrigger = document.querySelector('.sub-menu-coffee-trigger')
-   const subMenuCoffee = document.querySelector('.sub-menu.coffee')
+// $(document).ready(function () {
+//    // sub-menu coffee
+//    const subMenuCoffeeTrigger = document.querySelector('.sub-menu-coffee-trigger')
+//    const subMenuCoffee = document.querySelector('.sub-menu.coffee')
 
-   const showSubMenuCoffee = () => {
-      subMenuCoffee.style.display = 'block'
-      setTimeout(() => {
-         subMenuCoffee.style.opacity = 1
-         subMenuCoffee.style.transform = 'translateY(0)'
-      }, 0)
-   }
+//    const showSubMenuCoffee = () => {
+//       subMenuCoffee.style.display = 'block'
+//       setTimeout(() => {
+//          subMenuCoffee.style.opacity = 1
+//          subMenuCoffee.style.transform = 'translateY(0)'
+//       }, 0)
+//    }
 
-   const hideSubMenuCoffee = () => {
-      subMenuCoffee.style.opacity = 0
-      subMenuCoffee.style.transform = 'translateY(-101%)'
-      setTimeout(() => {
-         subMenuCoffee.style.display = 'none'
-      }, 510) // transition: 0.5s
-   }
+//    const hideSubMenuCoffee = () => {
+//       subMenuCoffee.style.opacity = 0
+//       subMenuCoffee.style.transform = 'translateY(-101%)'
+//       setTimeout(() => {
+//          subMenuCoffee.style.display = 'none'
+//       }, 510) // transition: 0.5s
+//    }
 
-   subMenuCoffeeTrigger.onmouseover = () => {
-      showSubMenuCoffee()
+//    subMenuCoffeeTrigger.onmouseover = () => {
+//       showSubMenuCoffee()
 
-      // hideSubMenuMerch
-      hideSubMenuMerch()
-   }
+//       // hideSubMenuMerch
+//       hideSubMenuMerch()
+//    }
 
-   subMenuCoffee.onmouseleave = () => {
-      hideSubMenuCoffee()
-   }
+//    subMenuCoffee.onmouseleave = () => {
+//       hideSubMenuCoffee()
+//    }
 
-   // sub-menu merch
-   const subMenuMerchTrigger = document.querySelector('.sub-menu-merch-trigger')
-   const subMenuMerch = document.querySelector('.sub-menu.merch')
+//    // sub-menu merch
+//    const subMenuMerchTrigger = document.querySelector('.sub-menu-merch-trigger')
+//    const subMenuMerch = document.querySelector('.sub-menu.merch')
 
-   const showSubMenuMerch = () => {
-      subMenuMerch.style.display = 'block'
-      setTimeout(() => {
-         subMenuMerch.style.opacity = 1
-         subMenuMerch.style.transform = 'translateY(0)'
-      }, 0)
-   }
+//    const showSubMenuMerch = () => {
+//       subMenuMerch.style.display = 'block'
+//       setTimeout(() => {
+//          subMenuMerch.style.opacity = 1
+//          subMenuMerch.style.transform = 'translateY(0)'
+//       }, 0)
+//    }
 
-   const hideSubMenuMerch = () => {
-      subMenuMerch.style.opacity = 0
-      subMenuMerch.style.transform = 'translateY(-101%)'
-      setTimeout(() => {
-         subMenuMerch.style.display = 'none'
-      }, 510) // transition: 0.5s
-   }
+//    const hideSubMenuMerch = () => {
+//       subMenuMerch.style.opacity = 0
+//       subMenuMerch.style.transform = 'translateY(-101%)'
+//       setTimeout(() => {
+//          subMenuMerch.style.display = 'none'
+//       }, 510) // transition: 0.5s
+//    }
 
-   subMenuMerchTrigger.onmouseover = () => {
-      showSubMenuMerch()
+//    subMenuMerchTrigger.onmouseover = () => {
+//       showSubMenuMerch()
 
-      // hideSubMenuCoffee
-      hideSubMenuCoffee()
-   }
+//       // hideSubMenuCoffee
+//       hideSubMenuCoffee()
+//    }
 
-   subMenuMerch.onmouseleave = () => {
-      hideSubMenuMerch()
-   }
-})
+//    subMenuMerch.onmouseleave = () => {
+//       hideSubMenuMerch()
+//    }
+// })
 
-$(document).ready(function () {
-   // navside
-   const navBarIcon = document.querySelector('.nav__icon.bar-icon')
-   const navCloseIcon = document.querySelector('.nav__icon.close-icon')
-   const navside = document.querySelector('.navside')
-   const navsideBody = document.querySelector('.navside__body')
-   let openingNavside = false
+// $(document).ready(function () {
+//    // navside
+//    const navBarIcon = document.querySelector('.nav__icon.bar-icon')
+//    const navCloseIcon = document.querySelector('.nav__icon.close-icon')
+//    const navside = document.querySelector('.navside')
+//    const navsideBody = document.querySelector('.navside__body')
+//    let openingNavside = false
 
-   const showNavside = () => {
-      navBarIcon.style.display = 'none'
-      navCloseIcon.style.display = 'block'
+//    const showNavside = () => {
+//       navBarIcon.style.display = 'none'
+//       navCloseIcon.style.display = 'block'
 
-      navside.style.display = 'block'
-      navsideBody.style.display = 'block'
+//       navside.style.display = 'block'
+//       navsideBody.style.display = 'block'
 
-      setTimeout(() => {
-         navside.style.opacity = 1
+//       setTimeout(() => {
+//          navside.style.opacity = 1
 
-         navsideBody.style.opacity = 1
-         navsideBody.style.left = 0
-      }, 0)
-   }
+//          navsideBody.style.opacity = 1
+//          navsideBody.style.left = 0
+//       }, 0)
+//    }
 
-   const hideNavside = () => {
-      navBarIcon.style.display = 'block'
-      navCloseIcon.style.display = 'none'
+//    const hideNavside = () => {
+//       navBarIcon.style.display = 'block'
+//       navCloseIcon.style.display = 'none'
 
-      navside.style.opacity = 0
-      navsideBody.style.opacity = 0
-      navsideBody.style.left = '-101%'
+//       navside.style.opacity = 0
+//       navsideBody.style.opacity = 0
+//       navsideBody.style.left = '-101%'
 
-      setTimeout(() => {
-         navsideBody.style.display = 'none'
-         navside.style.display = 'none'
-      }, 510) // transition 0.5s
-   }
+//       setTimeout(() => {
+//          navsideBody.style.display = 'none'
+//          navside.style.display = 'none'
+//       }, 510) // transition 0.5s
+//    }
 
-   navBarIcon.onclick = () => {
-      if (!openingNavside) {
-         showNavside()
+//    navBarIcon.onclick = () => {
+//       if (!openingNavside) {
+//          showNavside()
 
-         openingNavside = true
-         setTimeout(() => {
-            openingNavside = false
-         }, 510)
-      }
-   }
+//          openingNavside = true
+//          setTimeout(() => {
+//             openingNavside = false
+//          }, 510)
+//       }
+//    }
 
-   navCloseIcon.onclick = () => {
-      if (!openingNavside) {
-         hideNavside()
+//    navCloseIcon.onclick = () => {
+//       if (!openingNavside) {
+//          hideNavside()
 
-         openingNavside = true
-         setTimeout(() => {
-            openingNavside = false
-         }, 510)
-      }
-   }
+//          openingNavside = true
+//          setTimeout(() => {
+//             openingNavside = false
+//          }, 510)
+//       }
+//    }
 
-   // navside submenu coffee
-   const navsideSubmenuCoffeeTrigger = document.querySelector('.navside__submenu-coffee__trigger')
-   const navsideSubmenuCoffeeCloseBtn = document.querySelector('.navside__submenu-coffee__close-btn')
-   const navsideSubmenuCoffee = document.querySelector('.navside__submenu.coffee')
+//    // navside submenu coffee
+//    const navsideSubmenuCoffeeTrigger = document.querySelector('.navside__submenu-coffee__trigger')
+//    const navsideSubmenuCoffeeCloseBtn = document.querySelector('.navside__submenu-coffee__close-btn')
+//    const navsideSubmenuCoffee = document.querySelector('.navside__submenu.coffee')
 
-   const showNavsideSubmenuCoffee = () => {
-      navsideSubmenuCoffee.style.display = 'flex'
-      setTimeout(() => {
-         navsideSubmenuCoffee.style.right = 0
-      }, 0)
-   }
+//    const showNavsideSubmenuCoffee = () => {
+//       navsideSubmenuCoffee.style.display = 'flex'
+//       setTimeout(() => {
+//          navsideSubmenuCoffee.style.right = 0
+//       }, 0)
+//    }
 
-   const hideNavsideSubmenuCoffee = () => {
-      navsideSubmenuCoffee.style.right = '-101%'
-      setTimeout(() => {
-         navsideSubmenuCoffee.style.display = 'none'
-      }, 310) // transition: 0.3s
-   }
+//    const hideNavsideSubmenuCoffee = () => {
+//       navsideSubmenuCoffee.style.right = '-101%'
+//       setTimeout(() => {
+//          navsideSubmenuCoffee.style.display = 'none'
+//       }, 310) // transition: 0.3s
+//    }
 
-   navsideSubmenuCoffeeTrigger.onclick = () => {
-      showNavsideSubmenuCoffee()
-   }
+//    navsideSubmenuCoffeeTrigger.onclick = () => {
+//       showNavsideSubmenuCoffee()
+//    }
 
-   navsideSubmenuCoffeeCloseBtn.onclick = () => {
-      hideNavsideSubmenuCoffee()
-   }
+//    navsideSubmenuCoffeeCloseBtn.onclick = () => {
+//       hideNavsideSubmenuCoffee()
+//    }
 
-   // navside submenu merch
-   const navsideSubmenuMerchTrigger = document.querySelector('.navside__submenu-merch__trigger')
-   const navsideSubmenuMerchCloseBtn = document.querySelector('.navside__submenu-merch__close-btn')
-   const navsideSubmenuMerch = document.querySelector('.navside__submenu.merch')
+//    // navside submenu merch
+//    const navsideSubmenuMerchTrigger = document.querySelector('.navside__submenu-merch__trigger')
+//    const navsideSubmenuMerchCloseBtn = document.querySelector('.navside__submenu-merch__close-btn')
+//    const navsideSubmenuMerch = document.querySelector('.navside__submenu.merch')
 
-   const showNavsideSubmenuMerch = () => {
-      navsideSubmenuMerch.style.display = 'flex'
-      setTimeout(() => {
-         navsideSubmenuMerch.style.right = 0
-      }, 0)
-   }
+//    const showNavsideSubmenuMerch = () => {
+//       navsideSubmenuMerch.style.display = 'flex'
+//       setTimeout(() => {
+//          navsideSubmenuMerch.style.right = 0
+//       }, 0)
+//    }
 
-   const hideNavsideSubmenuMerch = () => {
-      navsideSubmenuMerch.style.right = '-101%'
-      setTimeout(() => {
-         navsideSubmenuMerch.style.display = 'none'
-      }, 310) // transition: 0.3s
-   }
+//    const hideNavsideSubmenuMerch = () => {
+//       navsideSubmenuMerch.style.right = '-101%'
+//       setTimeout(() => {
+//          navsideSubmenuMerch.style.display = 'none'
+//       }, 310) // transition: 0.3s
+//    }
 
-   navsideSubmenuMerchTrigger.onclick = () => {
-      showNavsideSubmenuMerch()
-   }
+//    navsideSubmenuMerchTrigger.onclick = () => {
+//       showNavsideSubmenuMerch()
+//    }
 
-   navsideSubmenuMerchCloseBtn.onclick = () => {
-      hideNavsideSubmenuMerch()
-   }
-})
+//    navsideSubmenuMerchCloseBtn.onclick = () => {
+//       hideNavsideSubmenuMerch()
+//    }
+// })
 
-$(document).ready(function () {
-   // recomment product
-   $('.cart-modal__product-item__body__add-btn').click(function () {
-      // Hiển thị select menu của form được nhấn
-      $(this).closest('.cart-modal__product-item').find('.cart-modal__product-item__selector').show()
+// $(document).ready(function () {
+//    // recomment product
+//    $('.cart-modal__product-item__body__add-btn').click(function () {
+//       // Hiển thị select menu của form được nhấn
+//       $(this).closest('.cart-modal__product-item').find('.cart-modal__product-item__selector').show()
 
-      setTimeout(() => {
-         $(this).attr('type', 'submit')
-      }, 0)
-   })
-})
+//       setTimeout(() => {
+//          $(this).attr('type', 'submit')
+//       }, 0)
+//    })
+// })
 
-$(document).ready(function () {
-   const plusCartBtn = $('.cart__quantity-btn.increase')
-   const minusCartBtn = $('.cart__quantity-btn.decrease')
+// $(document).ready(function () {
+//    const plusCartBtn = $('.cart__quantity-btn.increase')
+//    const minusCartBtn = $('.cart__quantity-btn.decrease')
 
-   plusCartBtn.click(function () {
-      const quantityWrap = $(this).closest(
-         '.cart-modal__product-no-empty-body__content__amount__btn-wraper'
-      )
-      const cartItemId = quantityWrap.data('cart-item-id')
-      const quantityCartBox = quantityWrap.find(
-         '.cart-modal__product-no-empty-body__content__amount__amount-show'
-      )
+//    plusCartBtn.click(function () {
+//       const quantityWrap = $(this).closest(
+//          '.cart-modal__product-no-empty-body__content__amount__btn-wraper'
+//       )
+//       const cartItemId = quantityWrap.data('cart-item-id')
+//       const quantityCartBox = quantityWrap.find(
+//          '.cart-modal__product-no-empty-body__content__amount__amount-show'
+//       )
 
-      $.ajax({
-         method: 'POST',
-         url: `/admin/cart/increase/${cartItemId}`,
-         success: function (data) {
-            quantityCartBox.text(data.newQuantity)
-         },
+//       $.ajax({
+//          method: 'POST',
+//          url: `/admin/cart/increase/${cartItemId}`,
+//          success: function (data) {
+//             quantityCartBox.text(data.newQuantity)
+//          },
 
-         error: function (err) {
-            console.log(err)
-         },
-      })
-   })
+//          error: function (err) {
+//             console.log(err)
+//          },
+//       })
+//    })
 
-   minusCartBtn.click(function () {
-      const quantityWrap = $(this).closest(
-         '.cart-modal__product-no-empty-body__content__amount__btn-wraper'
-      )
-      const cartItemId = quantityWrap.data('cart-item-id')
-      const quantityCartBox = quantityWrap.find(
-         '.cart-modal__product-no-empty-body__content__amount__amount-show'
-      )
+//    minusCartBtn.click(function () {
+//       const quantityWrap = $(this).closest(
+//          '.cart-modal__product-no-empty-body__content__amount__btn-wraper'
+//       )
+//       const cartItemId = quantityWrap.data('cart-item-id')
+//       const quantityCartBox = quantityWrap.find(
+//          '.cart-modal__product-no-empty-body__content__amount__amount-show'
+//       )
 
-      // prevent case the quantity = 0
-      if (+quantityCartBox.text().trim() - 1 > 0) {
-         $.ajax({
-            method: 'POST',
-            url: `/admin/cart/decrease/${cartItemId}`,
-            success: function (data) {
-               quantityCartBox.text(data.newQuantity)
-            },
+//       // prevent case the quantity = 0
+//       if (+quantityCartBox.text().trim() - 1 > 0) {
+//          $.ajax({
+//             method: 'POST',
+//             url: `/admin/cart/decrease/${cartItemId}`,
+//             success: function (data) {
+//                quantityCartBox.text(data.newQuantity)
+//             },
 
-            error: function (err) {
-               console.log(err)
-            },
-         })
-      }
-   })
-})
+//             error: function (err) {
+//                console.log(err)
+//             },
+//          })
+//       }
+//    })
+// })
